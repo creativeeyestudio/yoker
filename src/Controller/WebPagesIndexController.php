d51affb6d65e4e01b2340e2da809d10342895035
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-25 23:18:50 +0200

    KR - Création de la page d'accueil

--- src/Controller/WebPagesIndexController.php
+++ src/Controller/WebPagesIndexController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\PagesList;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WebPagesIndexController extends AbstractController
 {
-    #[Route('/web/pages/index', name: 'app_web_pages_index')]
-    public function index(): Response
+    #[Route('/fr', name: 'web_index')]
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('web_pages_index/index.html.twig', [
+        $index_page = $doctrine->getRepository(PagesList::class)->findBy(["page_url" => "index"]);
+
+        if (!$index_page) {
+            throw $this->createNotFoundException(
+                'La page d\'accueil du site est introuvable. Contactez le webmaster du site pour remédier au problème'
+            );
+        }
+
+        return $this->render('webpages/pages/accueil.html.twig', [
             'controller_name' => 'WebPagesIndexController',
         ]);
     }
