2241e5322d93ecb4363607137d05588bb97980cb
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-09-01 22:17:06 +0200

    KR - Création du controller

---
+++ src/Controller/WebPagesOthersController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PagesList;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebPagesOthersController extends AbstractController
+{
+    #[Route('/fr/{page_slug}', name: 'app_web_pages_others')]
+    public function index(ManagerRegistry $doctrine, string $page_slug): Response
+    {
+        $selected_page = $doctrine->getRepository(PagesList::class)->findBy(["page_url" => $page_slug]);
+
+        if (!$selected_page) {
+            throw $this->createNotFoundException(
+                'La page demandée est introuvable. Contactez le webmaster du site pour remédier au problème.'
+            );
+        }
+
+        return $this->render('web_pages_others/index.html.twig', [
+            'controller_name' => 'WebPagesOthersController',
+        ]);
+    }
+}
