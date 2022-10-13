7b6ef35bf93d43449eed95c6027522292a98fbbc
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-10-13 10:34:44 +0200

    KR - Changement du mot de passe

---
+++ src/Controller/AdminChangePasswordController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminChangePasswordController extends AbstractController
+{
+    #[Route('/admin/change-password', name: 'app_admin_change_password')]
+    public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $encoder): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+
+                $doctrine = $doctrine->getManager();
+                $doctrine->persist($user);
+                $doctrine->flush();
+            }
+        }
+
+        return $this->render('admin_change_password/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
