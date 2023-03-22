9234e5626200fe61f96f7d33b6da6f4fa1b186a3
Kévin R <rifakev@gmail.com>
2023-03-22 18:59:45 +0100

    KR - MAJ des dépendances

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,6 @@
-composer install
-chmod -R 777 /var/log/dev.log
-php bin/console cache:clear
+chmod -R 777 templates
 php bin/console doctrine:database:create
-php bin/console d:m:m --no-interaction
-php bin/console app:create-user hello@creative-eye.fr admin
+php bin/conssole make:migration
+php bin/console doctrine:migrations:migrate
+php bin/console app:user:create Kévin RIFA hello@creative-eye.fr admin
 php bin/console cache:clear
