6247e283f2b5f33d8fb707a06a8d052b13fe7747
Kévin R <rifakev@gmail.com>
2023-03-28 00:52:40 +0200

    KR - Ajout de Docker

--- docker/docker.sh
+++
@@ -1,6 +0,0 @@
-chmod -R 777 templates
-php bin/console doctrine:database:create
-php bin/conssole make:migration
-php bin/console doctrine:migrations:migrate
-php bin/console app:user:create Kévin RIFA hello@creative-eye.fr admin
-php bin/console cache:clear
