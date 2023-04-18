330df76d34c597a17b04e582cd807ddde3e72176
Kévin R <rifakev@gmail.com>
2023-04-19 00:46:20 +0200

    KR - Optimisation du code

--- src/Controller/WebPagesIndexController.php
+++ src/Controller/WebPagesIndexController.php
@@ -5,38 +5,25 @@ namespace App\Controller;
 use App\Entity\PagesList;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class WebPagesIndexController extends AbstractController
 {
+
     // Page Generator
     // -----------------------------------------------------------------------------------------------------------------
-    private function showPage(ManagerRegistry $doctrine, int $lang, string $page_id){
+    private function showPage(ManagerRegistry $doctrine, Request $request, string $page_id){
         $index_page = $doctrine->getRepository(PagesList::class)->findOneBy(["page_url" => $page_id]);
-
-        switch ($lang) {
-            case 0:
-                if ($index_page){
-                    $page_lang = 'fr';
-                    $meta_title = $index_page->getPageMetaTitle();
-                    $meta_desc = $index_page->getPageMetaDesc();
-                } else {
-                    return $this->redirectToRoute('web_index');
-                }
-                break;
-            case 1:
-                if ($index_page){
-                    $page_lang = 'en';
-                    $meta_title = $index_page->getPageMetaTitleEn();
-                    $meta_desc = $index_page->getPageMetaDescEn();
-                } else {
-                    return $this->redirectToRoute('web_index_en');
-                }
-                break;
-            default:
-                break;
+        
+        if ($index_page){
+            $page_lang = $request->getLocale();
+            $meta_title = $index_page->getPageMetaTitle();
+            $meta_desc = $index_page->getPageMetaDesc();
+        } else {
+            return $this->redirectToRoute('web_index');
         }
 
         return $this->render('web_pages_views/index.html.twig', [
@@ -50,39 +37,22 @@ class WebPagesIndexController extends AbstractController
 
     // Index Page
     // -----------------------------------------------------------------------------------------------------------------
-    #[Route('/fr', name: 'web_index')]
-    public function index_fr(ManagerRegistry $doctrine): Response
+    #[Route('/{_locale}', name: 'web_index', requirements: ['_locale' => 'fr|en'])]
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
-        $page = $this->showPage($doctrine, 0, 'index');
-        return $page;
-    }
-
-    #[Route('/en', name: 'web_index_en')]
-    public function index_en(ManagerRegistry $doctrine): Response
-    {
-        $page = $this->showPage($doctrine, 1, 'index');
+        $page = $this->showPage($doctrine, $request, 'index');
         return $page;
     }
 
 
     // Other Page
     // -----------------------------------------------------------------------------------------------------------------
-    #[Route('/fr/{page_slug}', name: 'web_page')]
-    public function page_fr(ManagerRegistry $doctrine, string $page_slug): Response
+    #[Route('/{_locale}/{page_slug}', name: 'web_page', requirements: ['_locale' => 'fr|en'])]
+    public function page(ManagerRegistry $doctrine, Request $request, string $page_slug): Response
     {
-        $page = $this->showPage($doctrine, 0, $page_slug);
+        $page = $this->showPage($doctrine, $request, $page_slug);
         if($page_slug == 'index')
-            return $this->redirectToRoute('web_index');
-        else
-            return $page;
-    }
-
-    #[Route('/en/{page_slug}', name: 'web_page_en')]
-    public function page_en(ManagerRegistry $doctrine, string $page_slug): Response
-    {
-        $page = $this->showPage($doctrine, 1, $page_slug);
-        if($page_slug == 'index')
-            return $this->redirectToRoute('web_index_en');
+            return $this->redirectBase();
         else
             return $page;
     }
