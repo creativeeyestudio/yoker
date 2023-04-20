6143e2ab0d715f43178d4946310f69b5c5254754
Kévin R <rifakev@gmail.com>
2023-04-20 18:10:08 +0200

    KR 6 Correction de la page admin files

--- src/Controller/AdminFilesController.php
+++ src/Controller/AdminFilesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Form\FileUploadFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +11,6 @@ class AdminFilesController extends AbstractController
     #[Route('/admin/medias', name: 'app_admin_files')]
     public function index(): Response
     {
-
-        $form = $this->createForm(FileUploadFormType::class);
-        return $this->render('admin_files/index.html.twig', [
-            'form' => $form->createView(),
-            'controller_name' => 'AdminFilesController',
-        ]);
+        return $this->render('admin_files/index.html.twig');
     }
 }
