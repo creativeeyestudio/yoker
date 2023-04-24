17004b78c1549766c200003e7b54af8e14f5fe6f
Kévin R <rifakev@gmail.com>
2023-04-24 20:21:09 +0200

    KR - MAJ de la page d'accueil

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\PagesList;
+use App\Entity\PostsList;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $em = $doctrine->getManager();
+        $pagesList = $em->getRepository(PagesList::class)->findBy([], ['id' => 'DESC'], 5);
+        $postsList = $em->getRepository(PostsList::class)->findBy([], ['id' => 'DESC'], 5);
+        $usersList = $em->getRepository(User::class)->findBy([], ['id' => 'DESC'], 5);
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'phpversion' => phpversion(),
+            'pagesList' => $pagesList,
+            'postsList' => $postsList,
+            'usersList' => $usersList,
         ]);
     }
 }
