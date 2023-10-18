690a7db865f7c3a45481420ad89076ba109a3dd6
Kévin R <rifakev@gmail.com>
2023-10-18 20:40:01 +0200

    KR - Corrections techniques

--- src/Controller/WebPagesIndexController.php
+++ src/Controller/WebPagesIndexController.php
@@ -11,40 +11,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class WebPagesIndexController extends AbstractController
 {
     private $pages_services;
+    private $request;
 
     public function __construct(PagesService $pages_services)
     {
         $this->pages_services = $pages_services;
+        $this->request = new Request();
     }
     
     // Index Page
-    // -----------------------------------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
     #[Route('/{_locale}', name: 'web_index', requirements: ['_locale' => 'fr'])]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        return $this->pages_services->getMainPage($request);
+        return $this->pages_services->getMainPage($this->request);
     }
 
     // Other Page
-    // -----------------------------------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
     #[Route('/{_locale}/{page_slug}', name: 'web_page', requirements: ['_locale' => 'fr'])]
-    public function page(Request $request, string $page_slug): Response
+    public function page(string $page_slug): Response
     {
-        return $this->pages_services->getPage($request, $page_slug);
+        return $this->pages_services->getPage($this->request, $page_slug);
     }
     
     // Post Page
-    // -----------------------------------------------------------------------------------------------------------------
-    #[Route('/{_locale}/blog/{post_url}', name: 'web_post', requirements: ['_locale' => 'fr|en'])]
-    public function post(string $post_url, Request $request): Response
+    // -------------------------------------------------------------------------------------------
+    #[Route('/{_locale}/blog/{post_slug}', name: 'web_post', requirements: ['_locale' => 'fr|en'])]
+    public function post(string $post_slug): Response
     {
-        return $this->pages_services->getPost($request, $post_url);
+        return $this->pages_services->getPost($this->request, $post_slug);
     }
 
     // Redirections
-    // -----------------------------------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
     #[Route('/', name: 'web_redirect')]
     public function redirectBase(){
         return $this->redirectToRoute('web_index');
     }
+
+    // API
+    // -------------------------------------------------------------------------------------------
 }
